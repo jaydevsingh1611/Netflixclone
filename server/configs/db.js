f56e18a2630faa87f0deee18f2ba630aaa339459
jaydevsingh1611 <jaydevsingh1611@gmail.com>
2025-12-29 22:25:36 +0530

    Your commit message

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,11 +1,32 @@
 import mongoose from 'mongoose';
+
+let isConnected = false;
+
 export const connectDB = async () => {
     try {
+        // If already connected, return early
+        if (mongoose.connection.readyState === 1) {
+            isConnected = true;
+            return;
+        }
+
+        if (isConnected) {
+            return;
+        }
+
         mongoose.connection.on('connected', () => {
             console.log("Database Connected");
+            isConnected = true;
         });
+
+        mongoose.connection.on('error', (err) => {
+            console.log("DB Error:", err.message);
+            isConnected = false;
+        });
+
         await mongoose.connect(`${process.env.MONGODB_URI}/netShow`);
     } catch (error) {
         console.log("DB Error:", error.message);
+        isConnected = false;
     }
 }
---
+++ server/inngest/index.js
@@ -0,0 +1,53 @@
+import { Inngest } from "inngest";
+import User from "../models/User.js";
+import { Booking } from "../models/Booking.js";
+import Show from "../models/Show.js";
+
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "movie-ticket-booking" });
+
+//Inngest Function to save user data to a database
+const syncUserCreation = inngest.createFunction(
+    { id: 'sync-user-from-clerk' },
+    { event: 'clerk/user.created' },
+    async ({ event }) => {
+        const { id, first_name, last_name, email_addresses, image_url } = event.data
+        const userData = {
+            _id: id,
+            email: email_addresses[0].email_address,
+            name: first_name + ' ' + last_name,
+            image: image_url
+        }
+        await User.create(userData)
+    }
+)
+
+//Inngest Function to delete user from a  database
+const syncUserDeletion = inngest.createFunction(
+    { id: 'delete-user-with-clerk' },
+    { event: 'clerk/user.deleted' },
+    async ({ event }) => {
+        const { id } = event.data
+        await User.findByIdAndDelete(id)
+    }
+)
+
+//Inngest Function to Update user from a  database
+const syncUserUpdation = inngest.createFunction(
+    { id: 'update-user-from-clerk' },
+    { event: 'clerk/user.updated' },
+    async ({ event }) => {
+        const { id, first_name, last_name, email_addresses, image_url } = event.data
+        const userData = {
+            _id: id,
+            email: email_addresses[0].email_address,
+            name: first_name + ' ' + last_name,
+            image: image_url
+        }
+        await User.findByIdAndUpdate(id, userData)
+    }
+)
+// Inngest Function to cancel booking and release seats of show after 10 minutes of booking created if payment is not made
+// Create an empty array where we'll export future Inngest functions
+export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdation];
